d5f5fe9380ab33b4e37b49194f57ab0e3fd1ac7b
bennybop <bennybop74@yahoo.com>
2017-09-30 20:44:55 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -5,12 +5,7 @@ def display_board
 
   puts "A Tic Tac Toe Board"
 
-
-  puts " #{cell[0]} | #{cell[1]} | #{cell[2]} ",
-  puts "-----------",
-  puts " #{cell[3]} | #{cell[4]} | #{cell[5]} ",
-  puts "-----------",
-  puts " #{cell[6]} | #{cell[7]} | #{cell[8]} "
+puts "   |   |   ", "-----------", "   |   |   "
 
 
 
